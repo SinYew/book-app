06da2815af3f458d9b6c7e2d14d8fffb01aa676e
LohSinYew <lohsinyew@gmail.com>
2022-06-10 15:58:06 +0800

    Add users data

---
+++ src/data/users.ts
@@ -0,0 +1,9 @@
+import { UserState } from "../features/users/usersSlice";
+
+export const users: UserState[] = [
+  { id: "1", name: "Alexis Hickle", role: "admin", dateJoined: "2022-03-15" },
+  { id: "2", name: "Clare Durgan", role: "editor", dateJoined: "2022-04-10" },
+  { id: "3", name: "Pierre Greenholt", role: "user", dateJoined: "2022-04-14" },
+  { id: "4", name: "Tess Beatty", role: "user", dateJoined: "2022-04-20" },
+  { id: "5", name: "Moses Legros", role: "user", dateJoined: "2022-05-01" },
+];
