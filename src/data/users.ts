db43727c6726fde41e269f0810905dbd0d8437f7
LohSinYew <lohsinyew@gmail.com>
2022-06-11 21:37:34 +0800

    update users data

--- src/data/users.ts
+++ src/data/users.ts
@@ -1,9 +1,16 @@
 import { UserState } from "../features/users/usersSlice";
 
 export const users: UserState[] = [
-  { id: "1", name: "Alexis Hickle", role: "admin", dateJoined: "2022-03-15" },
-  { id: "2", name: "Clare Durgan", role: "editor", dateJoined: "2022-04-10" },
-  { id: "3", name: "Pierre Greenholt", role: "user", dateJoined: "2022-04-14" },
-  { id: "4", name: "Tess Beatty", role: "user", dateJoined: "2022-04-20" },
-  { id: "5", name: "Moses Legros", role: "user", dateJoined: "2022-05-01" },
+  { id: "-wPwed35Pjz_aDjdVY11I", name: "Alexis Hickle", role: "admin", dateJoined: "2022-03-15" },
+  { id: "D-ND0_jiP3LqlWrJsVIPq", name: "Clare Durgan", role: "editor", dateJoined: "2022-04-10" },
+  { id: "DpTtcJAXkI6f3QzED_Bd2", name: "Pierre Greenholt", role: "user", dateJoined: "2022-04-14" },
+  { id: "HDZy46Vd6gpV-G0HQ2ZCQ", name: "Tess Beatty", role: "user", dateJoined: "2022-04-20" },
+  { id: "MCJgHk4rfhMxrmrRGlKIn", name: "Moses Legros", role: "user", dateJoined: "2022-05-01" },
+  { id: "bvh3eHrxu0tS79_r-9b6H", name: "Jamil Azher", role: "user", dateJoined: "2022-05-01" },
+  { id: "dJS-NV5yq9DAjdOrjp3K-", name: "James Bannan", role: "user", dateJoined: "2022-05-01" },
+  { id: "qiDba-Lv0MypgrkR87sbE", name: "Avi Pfeffer", role: "user", dateJoined: "2022-05-01" },
+  { id: "rgCxTgvSXVySumb2kVr2K", name: "Joseph Hocking", role: "user", dateJoined: "2022-05-01" },
+  { id: "uLCw0w5H_1NIZS8O-RyYk", name: "Jon Skeet", role: "user", dateJoined: "2022-05-01" },
+  { id: "ualT63iVyHcBtqy5uJMIN", name: "Daniel Brolund", role: "user", dateJoined: "2022-05-01" },
+  { id: "webSOooCtcdeSThOO2_Qd", name: "Alex Young", role: "user", dateJoined: "2022-05-01" },
 ];
