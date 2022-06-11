48e090d6e1384a40584b164ae32f96f870c21e70
LohSinYew <lohsinyew@gmail.com>
2022-06-12 04:48:33 +0800

    add books borrow return functionality

---
+++ src/components/book/BorrowReturn.tsx
@@ -0,0 +1,114 @@
+import { Button, Grid } from "@mui/material";
+import { ChangeEvent, useState } from "react";
+import { useAppDispatch, useAppSelector } from "../../app/hooks";
+import { BookState, bookUpdated, selectBooks } from "../../features/books/booksSlice";
+import { bookBorrow, bookReturn, selectTransactions, TransactionState } from "../../features/books/transactionSlice";
+import { selectCurrentUser } from "../../features/users/currentUserSlice";
+import BorrowReturnPopup from "./BorrowReturnPopup";
+
+const BorrowReturn = () => {
+  const books = useAppSelector(selectBooks);
+  const transactions = useAppSelector(selectTransactions);
+  const currentUser = useAppSelector(selectCurrentUser);
+  const dispatch = useAppDispatch();
+
+  const [open, setOpen] = useState(false);
+  const [status, setStatus] = useState("");
+  const [isbn, setIsbn] = useState("");
+
+  const onBorrow = () => {
+    setStatus("borrow");
+    setOpen(true);
+  };
+
+  const onReturn = () => {
+    setStatus("return");
+    setOpen(true);
+  };
+
+  const onChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const value = event.target.value;
+    setIsbn(value);
+  };
+
+  const applyChanges = () => {
+    const book = books.find((book: BookState) => book.isbn === isbn);
+
+    if (book) {
+      const borrowTransaction = transactions.find((transaction: TransactionState) => {
+        return transaction.bookId === book.id && transaction.status === "borrow"
+      });
+
+      const returnTransaction = transactions.find((transaction: TransactionState) => {
+        return transaction.bookId === book.id && transaction.userId === currentUser.id && transaction.status === "borrow"
+      });
+
+      if (status === "borrow") {
+        if (borrowTransaction) {
+          alert("this book is not available");
+          return;
+        }
+        const startingId = transactions.length > 0 ? transactions[transactions.length - 1].id + 1 : 0;
+
+        const newTransaction: TransactionState = {
+          id: startingId,
+          bookId: book.id,
+          userId: currentUser.id,
+          status: "borrow"
+        }
+
+        dispatch(bookBorrow(newTransaction));
+        dispatch(bookUpdated({
+          ...book,
+          borrowAvailability: false,
+          lastBorrower: currentUser.name
+        }))
+      } else {
+
+        if (returnTransaction) {
+          dispatch(bookReturn({
+            id: returnTransaction.id,
+            status: "return"
+          }));
+
+          dispatch(bookUpdated({
+            ...book,
+            borrowAvailability: true
+          }))
+        } else {
+          alert("Invalid request");
+        }
+
+      }
+
+      setOpen(false);
+
+    } else {
+      alert("this book does not exist");
+    }
+
+    setIsbn("");
+  };
+
+  const cancelChanges = () => {
+    setOpen(false);
+  };
+
+  return (
+    <>
+      <Grid item xs={12}>
+        <Button variant="contained" sx={{ mr: 3 }} onClick={onBorrow}>Borrow</Button>
+        <Button variant="contained" color="secondary" onClick={onReturn}>Return</Button>
+      </Grid>
+      <BorrowReturnPopup
+        open={open}
+        status={status}
+        onChange={onChange}
+        onApplyChanges={applyChanges}
+        onCancelChanges={cancelChanges}
+      />
+    </>
+  )
+};
+
+export default BorrowReturn
