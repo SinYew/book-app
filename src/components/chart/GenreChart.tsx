895c24a9de2f219c2172ea5ed0d7145dc0e20ec6
LohSinYew <lohsinyew@gmail.com>
2022-06-12 00:39:46 +0800

    update chart component

--- src/components/chart/GenreChart.tsx
+++ src/components/chart/GenreChart.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import { Grid, Paper } from '@mui/material';
 import {
   Chart as ChartJS,
@@ -10,7 +9,8 @@ import {
   Legend,
 } from 'chart.js';
 import { Bar } from 'react-chartjs-2';
-import { books } from '../../data/books';
+import { useAppSelector } from '../../app/hooks';
+import { selectBooks } from '../../features/books/booksSlice';
 
 ChartJS.register(
   CategoryScale,
@@ -21,45 +21,47 @@ ChartJS.register(
   Legend
 );
 
-// get label from books store
-const labels = books.map(book => book.genre)
-  .filter((value, index, self) => self.indexOf(value) === index);
-
-// get book counts based on genre
-const booksData = books.reduce((previous: any, current) => {
-  const genre = current.genre;
-  if (!previous.hasOwnProperty(genre)) {
-    previous[genre] = 0;
-  }
-  previous[genre]++;
-  return previous;
-}, {});
-
-export const options = {
-  responsive: true,
-  plugins: {
-    legend: {
-      position: 'top' as const,
-    },
-    title: {
-      display: true,
-      text: 'Books by genre',
-    },
-  },
-};
-
-export const data = {
-  labels,
-  datasets: [
-    {
-      label: 'Books',
-      data: booksData,
-      backgroundColor: 'rgba(255, 99, 132, 0.5)',
-    }
-  ],
-};
-
 const GenreChart = () => {
+
+  const books = useAppSelector(selectBooks);
+
+  // get label from books store
+  const labels = books.map(book => book.genre)
+    .filter((value, index, self) => self.indexOf(value) === index);
+
+  // get book counts based on genre
+  const booksData = books.reduce((previous: any, current) => {
+    const genre = current.genre;
+    if (!previous.hasOwnProperty(genre)) {
+      previous[genre] = 0;
+    }
+    previous[genre]++;
+    return previous;
+  }, {});
+
+  const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: 'top' as const,
+      },
+      title: {
+        display: true,
+        text: 'Books by genre',
+      },
+    },
+  };
+
+  const data = {
+    labels,
+    datasets: [
+      {
+        label: 'Books',
+        data: booksData,
+        backgroundColor: 'rgba(255, 99, 132, 0.5)',
+      }
+    ],
+  };
 
   return (
     <Grid item xs={12} xl={10} maxWidth={"lg"}>
--- src/components/chart/YearPublishChart.tsx
+++ src/components/chart/YearPublishChart.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import { Grid, Paper } from '@mui/material';
 import {
   Chart as ChartJS,
@@ -10,7 +9,8 @@ import {
   Legend,
 } from 'chart.js';
 import { Bar } from 'react-chartjs-2';
-import { books } from '../../data/books';
+import { useAppSelector } from '../../app/hooks';
+import { selectBooks } from '../../features/books/booksSlice';
 
 ChartJS.register(
   CategoryScale,
@@ -21,45 +21,47 @@ ChartJS.register(
   Legend
 );
 
-// get label from books store
-const labels = books.map(book => book.yearPublished)
-  .filter((value, index, self) => self.indexOf(value) === index).sort();
-
-// get book counts based on year published
-const booksData = books.reduce((previous: any, current) => {
-  const year = current.yearPublished;
-  if (!previous.hasOwnProperty(year)) {
-    previous[year] = 0;
-  }
-  previous[year]++;  
-  return previous;
-}, {});
-
-export const options = {
-  responsive: true,
-  plugins: {
-    legend: {
-      position: 'top' as const,
-    },
-    title: {
-      display: true,
-      text: 'Books by year published',
-    },
-  },
-};
-
-export const data = {
-  labels,
-  datasets: [
-    {
-      label: 'Books',
-      data: booksData,
-      backgroundColor: 'rgba(53, 162, 235, 0.5)',
-    }
-  ],
-};
-
 const YearPublishChart = () => {
+
+  const books = useAppSelector(selectBooks);
+
+  // get label from books store
+  const labels = books.map(book => book.yearPublished)
+    .filter((value, index, self) => self.indexOf(value) === index).sort();
+
+  // get book counts based on year published
+  const booksData = books.reduce((previous: any, current) => {
+    const year = current.yearPublished;
+    if (!previous.hasOwnProperty(year)) {
+      previous[year] = 0;
+    }
+    previous[year]++;
+    return previous;
+  }, {});
+
+  const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: 'top' as const,
+      },
+      title: {
+        display: true,
+        text: 'Books by year published',
+      },
+    },
+  };
+
+  const data = {
+    labels,
+    datasets: [
+      {
+        label: 'Books',
+        data: booksData,
+        backgroundColor: 'rgba(53, 162, 235, 0.5)',
+      }
+    ],
+  };
 
   return (
     <Grid item xs={12} xl={10} maxWidth={"lg"}>
